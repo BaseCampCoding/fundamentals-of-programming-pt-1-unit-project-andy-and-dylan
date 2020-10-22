2a57a2feafa5f4b0515bd4eabe6e0bdb704f602e
Dylan McCreary <72216783+DylanMcC3525@users.noreply.github.com>
2020-10-22 15:42:02 -0500

    deleted files that werent for this

--- exercise_1.py
+++
@@ -1,8 +0,0 @@
-name_of_file = input('What is the name of the file?')
-newfile = name_of_file + '.bak'
-backup = open(newfile, 'w')
-
-for line in open(name_of_file):
-    backup.write(line)
-
-backup.close()
